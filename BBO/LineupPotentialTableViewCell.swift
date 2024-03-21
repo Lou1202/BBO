f833ae834050b84752d3477be8956e61b0d1aab0
Lou <hover1202@gmail.com>
2024-03-21 23:11:18 +0800

    完成切換Segmented切換Container View

---
+++ BBO/LineupPotentialTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LineupPotentialTableViewCell.swift
+//  BBO
+//
+//  Created by 楊曜安 on 2024/3/21.
+//
+
+import UIKit
+
+class LineupPotentialTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
